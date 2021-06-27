f5c48a42b238b9d2745b1f7a7059a4c6520756dd
developer-junaid <qjunaid623@gmail.com>
2021-06-27 13:00:55 +0500

    added identity-widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// Authentication
+import netlifyIdentity from "netlify-identity-widget";
 
 // Theme-ui
 import { Container, Heading, Button, Flex } from "theme-ui";
 
-const Home = (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2, cursor: "pointer" }}
-        onClick={() => {
-          alert("clicked");
-        }}
-      >
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-);
+const Home = (props) => {
+  useEffect(() => {
+    // Initialize netlify-identity
+    netlifyIdentity.init({});
+  }, []);
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2, cursor: "pointer" }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
 
 export default Home;
